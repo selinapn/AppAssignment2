7590fb8f61954ce71713d9612248c153c3ae0eb5
selinapn <57070340+selinapn@users.noreply.github.com>
2020-06-16 10:24:05 -0700

    Week 8

--- app/src/main/java/com/selinapn/appassignment2/details/ForecastDetailsFragment.kt
+++ app/src/main/java/com/selinapn/appassignment2/details/ForecastDetailsFragment.kt
@@ -5,20 +5,29 @@ import android.view.*
 import android.widget.TextView
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.selinapn.appassignment2.*
 import coil.api.load
+import com.selinapn.appassignment2.databinding.FragmentForecastDetailsBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 
-private val DATE_FORMAT  = SimpleDateFormat("MM-dd-YYYY")
-
 class ForecastDetailsFragment : Fragment() {
 
-
     private val args: ForecastDetailsFragmentArgs by navArgs()
+
+    private lateinit var viewModelFactory : ForecastDetailsViewModelFactory
+    private val viewModel: ForecastDetailsViewModel by viewModels(
+        factoryProducer = { viewModelFactory }
+    )
+
+    private var _binding: FragmentForecastDetailsBinding? = null
+    private val binding get()= _binding!!
+
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
 
@@ -27,25 +36,31 @@ class ForecastDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_forecast_details, container, false)
 
+        _binding = FragmentForecastDetailsBinding.inflate(inflater, container, false)
+        viewModelFactory = ForecastDetailsViewModelFactory(args)
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
-
-        val tempText = layout.findViewById<TextView>(R.id.tempText)
-        val descriptionText = layout.findViewById<TextView>(R.id.descriptionText)
-        val dateText = layout.findViewById<TextView>(R.id.dateText)
-        val forecastIcon = layout.findViewById<ImageView>(R.id.forecastIcon)
-
-        tempText.text = formatTempForDisplay(args.temp, tempDisplaySettingManager.getTempDisplaySetting())
-        descriptionText.text= args.description
-        dateText.text = DATE_FORMAT.format(Date(args.date*1000))
-        forecastIcon.load("http://openweathermap.org/img/wn/${args.icon}@2x.png")
-
-
-        return layout
+        return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewStateObserver = Observer<ForecastDetailsViewState> { viewState ->
+            //update the UI
+            binding.tempText.text = formatTempForDisplay(viewState.temp, tempDisplaySettingManager.getTempDisplaySetting())
+            binding.descriptionText.text = viewState.description
+            binding.dateText.text = viewState.date
+            binding.forecastIcon.load(viewState.iconUrl)
+        }
+        viewModel.viewState.observe(viewLifecycleOwner, viewStateObserver)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     }
---
+++ app/src/main/java/com/selinapn/appassignment2/details/ForecastDetailsViewState.kt
@@ -0,0 +1,8 @@
+package com.selinapn.appassignment2.details
+
+data class ForecastDetailsViewState (
+    val temp: Float,
+    val description: String,
+    val date: String,
+    val iconUrl: String
+)
